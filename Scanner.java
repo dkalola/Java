ffe9d23d76bd157355af89a9cbfa08de007b20b1
Divyanshu <divyanshukalola88@gmail.com>
2019-02-26 15:05:47 -0600

    Update and rename Lab3PartB.java to Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -4,7 +4,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Lab3PartB {
+public class Scanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
